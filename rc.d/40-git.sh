5773ce5a22b77a2b07a186dc4c73e65bda0b9f85
Tomoyuki Kano <tomo@appletz.jp>
2012-12-20 15:13:01 +0900

    update

--- rc.d/40-git.sh
+++ rc.d/40-git.sh
@@ -2,67 +2,72 @@
 # 40-git
 #
 
-if [[ -r $ZDOTDIR/lib/git-completion.bash ]]; then
-    . $ZDOTDIR/lib/git-completion.bash
-fi
-
+# if [[ -r $ZDOTDIR/lib/git-completion.bash ]]; then
+#     . $ZDOTDIR/lib/git-completion.bash
+# fi
 
 ######################################################################
 
-# # vcs_info
-# autoload vcs_info
-
-# zstyle ":vcs_info:*" enable git			# gitのみ有効にする
-# zstyle ":vcs_info:git:*" check-for-changes true			# commitしていない変更をチェックする
-# zstyle ":vcs_info:git:*" formats "%c%u[%b:%r]"			# 変更情報とリポジトリ情報を表示
-# zstyle ":vcs_info:git:*" actionformats "%c%u<%a>[%b:%r]"	# コンフリクトなどの情報を表示
-# zstyle ":vcs_info:git:*" unstagedstr "*"			# addしていない変更がある
-# zstyle ":vcs_info:git:*" stagedstr "+"				# commitしていないstageがある
-
-# # git：まだpushしていないcommitあるかチェックする
-# function my_git_info_push () {
-#     if [[ -n "$(git remote 2>/dev/null)" ]]; then
-# 	local head="$(git rev-parse HEAD)"
-# 	local remote
-# 	for remote in $(git rev-parse --remotes) ; do
-# 	    if [ "$head" = "$remote" ]; then return 0 ; fi
-# 	done
-# 	# pushしていないcommitがあることを示す文字列
-# 	# echo "<P>"
-#     fi
-# }
-
-# # git：stashに退避したものがあるかチェックする
-# my_git_info_stash () {
-#     if [[ -n "$(git stash list 2>/dev/null)" ]]; then
-# 	# stashがあることを示す文字列
-# 	# echo "{s}"
-# 	:
-#     fi
-# }
-
-# # vcs_infoの出力に独自の出力を付加する
-# my_vcs_info () {
-# 	vcs_info
-# 	echo "$(my_git_info_stash)$(my_git_info_push)$vcs_info_msg_0_"
-# }
-
-
-# ######################################################################
-
 local csi=$'\e['
 
-if [[ -r $ZDOTDIR/lib/git-prompt.sh ]]; then
+if zstyle -t ':zkit:' vcs_info; then
+    if [[ -r $ZDOTDIR/lib/git-prompt.sh ]]; then
 
-    . $ZDOTDIR/lib/git-prompt.sh
-    GIT_PS1_SHOWDIRTYSTATE=yes
-    GIT_PS1_SHOWSTASHSTATE=yes
-    GIT_PS1_SHOWUNTRACKEDFILES=yes
-    GIT_PS1_SHOWUPSTREAM=auto
+	. $ZDOTDIR/lib/git-prompt.sh
+	GIT_PS1_SHOWDIRTYSTATE=yes
+	GIT_PS1_SHOWSTASHSTATE=yes
+	GIT_PS1_SHOWUNTRACKEDFILES=yes
+	GIT_PS1_SHOWUPSTREAM=auto
+
+	if zstyle -t ':zkit:' rprompt; then
+	    RPROMPT="%{${csi}48;5;224m%}\$(__git_ps1 \"(%s)\")%{${csi}m%}$RPROMPT"
+	else
+	    PS1="%{${csi}48;5;224m%}\$(__git_ps1 \"(%s)\")%{${csi}m%}$PS1"
+	fi
+    fi
+else
+    # vcs_info
+    autoload -Uz vcs_info
+
+    zstyle ":vcs_info:*" enable git	# gitのみ有効にする
+    zstyle ":vcs_info:git:*" check-for-changes true		# commitしていない変更をチェックする
+    zstyle ":vcs_info:git:*" formats "%b%u%c"			# 変更情報とリポジトリ情報を表示
+    zstyle ":vcs_info:git:*" actionformats "%b%u%c<%a>"		# コンフリクトなどの情報を表示
+    zstyle ":vcs_info:git:*" stagedstr "+"			# commitしていないstageがある(%c)
+    zstyle ":vcs_info:git:*" unstagedstr "*"			# addしていない変更がある(%u)
+
+    # git：まだpushしていないcommitあるかチェックする
+    function __git_info_push () {
+	if [[ -n "$(git remote 2>/dev/null)" ]]; then
+	    local head="$(git rev-parse HEAD)"
+	    local remote
+	    for remote in $(git rev-parse --remotes) ; do
+		if [ "$head" = "$remote" ]; then return 0 ; fi
+	    done
+	    # pushしていないcommitがあることを示す文字列
+	    echo "^"
+	fi
+    }
+
+    # git：stashに退避したものがあるかチェックする
+    function __git_info_stash () {
+	if [[ -n "$(git stash list 2>/dev/null)" ]]; then
+	    # stashがあることを示す文字列
+	    echo "\$"
+	fi
+    }
+
+    # vcs_infoの出力に独自の出力を付加する
+    function __vcs_info () {
+	LANG=en_US.UTF-8 vcs_info
+	if [[ -n "${vcs_info_msg_0_}" ]]; then
+	    echo "(${vcs_info_msg_0_}$(__git_info_stash)$(__git_info_push))"
+	fi
+    }
 
     if zstyle -t ':zkit:' rprompt; then
-	RPROMPT="%{${csi}48;5;224m%}\$(__git_ps1 \"(%s)\")%{${csi}m%}$RPROMPT"
+	RPROMPT="%{${csi}48;5;224m%}\$(__vcs_info \"(%s)\")%{${csi}m%}$RPROMPT"
     else
-	PS1="%{${csi}48;5;224m%}\$(__git_ps1 \"(%s)\")%{${csi}m%}$PS1"
+	PS1="%{${csi}48;5;224m%}\$(__vcs_info \"(%s)\")%{${csi}m%}$PS1"
     fi
 fi
