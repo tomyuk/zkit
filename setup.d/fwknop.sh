68334db869743cd4d0f3c9327df100ac5f2c0e26
Tomoyuki Kano <tomo@appletz.jp>
2012-12-28 05:45:23 +0900

    update

--- setup.d/fwknop.sh
+++ setup.d/fwknop.sh
@@ -3,6 +3,9 @@
 # setup.d/fwknop.sh
 #
 
+: ${FWKNOP_ALLOW_IP:=0.0.0.0}
+: ${FWKNOP_USER:=fwkopclient}
 : ${FWKNOP_PASSWD_FILE:=${ZKIT_PRIVATE}/secrets/fwknop_passwd}
 
+# TODO: auto generate
 __zkit_template templates/fwknoprc.tmpl ${HOME}/.fwknoprc 600
