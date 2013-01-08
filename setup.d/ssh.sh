688fd28d57f6cacdb9ffc6344026c816c6724828
Tomoyuki Kano <tomo@appletz.jp>
2013-01-08 15:00:17 +0900

    add authorized_keys

--- setup.d/ssh.sh
+++ setup.d/ssh.sh
@@ -7,6 +7,8 @@ if [[ ! -d $ssh_dir ]]; then
     mkdir $ssh_dir
 fi
 
+## build ssh_config
+
 : ${SSH_FORWARD_AGENT:=no}
 
 typeset -a HOSTS
@@ -17,5 +19,13 @@ if [[ -r $hosts_conf ]]; then
     __zkit_readarray HOSTS_CONF < ${hosts_conf}
 fi
 __zkit_template templates/ssh_config.tmpl ${ssh_dir}/config 600
+
+## place authorized_keys
+
+if [[ -s ${ZKIT_PRIVATE}/secrets/authorized_keys ]]; then
+install -m 600 ${ZKIT_PRIVATE}/secrets/authorized_keys $ssh_dir/
+fi
+
+## house keeping
 
 chmod -R og-rwx $ssh_dir
