e615fbf1c99da01c8856464f85cb0fc93ee84d3c
Tomoyuki Kano <tomo@appletz.jp>
2013-01-15 10:36:58 +0900

    update

--- rc.d/33-java.sh
+++ rc.d/33-java.sh
@@ -1,10 +1,26 @@
 # for Sun JDK
 
-envpathmunge JAVA_HOME "/usr/java/default"
-if [[ -n "$JAVA_HOME" ]]; then
-    export CLASSPATH=".:${ZKIT_PRIVATE}/java:${JAVA_HOME}/lib:${JAVA_HOME}/jre/lib:/usr/share/java"
+JAVA_HOME=$(echo /usr/lib/jvm/java-*-openjdk-* | sort -r | head -1)
 
-    envpathmunge M2_HOME /opt/maven
-    envpathmunge TOMCATHOME /opt/tomcat
-    envpathmunge ANTHOME /opt/ant
+if [[ -z $JAVA_HOME ]]; then
+    if [[ -d /usr/java/default ]]; then
+	JAVA_HOME="/usr/java/default"
+    fi
 fi    
+
+if [[ -n $JAVA_HOME ]]; then
+    export JAVA_HOME
+    export CLASSPATH=".:${ZKIT_PRIVATE}/java:${JAVA_HOME}/lib:${JAVA_HOME}/jre/lib:/usr/share/java"
+    pathmunge ${JAVA_HOME}/jre/bin
+
+    # TODO: move to private
+    if [[ -d /opt/maven ]]; then
+	envpathmunge M2_HOME /opt/maven
+    fi
+    if [[ -d /opt/tomcat ]]; then
+	envpathmunge TOMCATHOME /opt/tomcat
+    fi
+    if [[ -d /opt/ant ]]; then
+	envpathmunge ANTHOME /opt/ant
+    fi
+fi
