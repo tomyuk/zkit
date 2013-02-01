a7247df2c4df565c72c37c1956ba38880d2fc698
Tomoyuki Kano <tomo@appletz.jp>
2013-02-01 17:15:11 +0900

    update

--- rc.d/33-java.sh
+++ rc.d/33-java.sh
@@ -1,26 +1,28 @@
 # for Sun JDK
 
-JAVA_HOME=$(echo /usr/lib/jvm/java-*-openjdk-* | sort -r | head -1)
+if [[ -d /usr/lib/jvm ]]; then
+    JAVA_HOME=$(echo /usr/lib/jvm/java-*-openjdk-* | sort -r | head -1)
 
-if [[ -z $JAVA_HOME ]]; then
-    if [[ -d /usr/java/default ]]; then
-	JAVA_HOME="/usr/java/default"
-    fi
-fi    
+    if [[ -z $JAVA_HOME ]]; then
+	if [[ -d /usr/java/default ]]; then
+	    JAVA_HOME="/usr/java/default"
+	fi
+    fi    
 
-if [[ -n $JAVA_HOME ]]; then
-    export JAVA_HOME
-    export CLASSPATH=".:${ZKIT_PRIVATE}/java:${JAVA_HOME}/lib:${JAVA_HOME}/jre/lib:/usr/share/java"
-    pathmunge ${JAVA_HOME}/jre/bin
+    if [[ -n $JAVA_HOME ]]; then
+	export JAVA_HOME
+	export CLASSPATH=".:${ZKIT_PRIVATE}/java:${JAVA_HOME}/lib:${JAVA_HOME}/jre/lib:/usr/share/java"
+	pathmunge ${JAVA_HOME}/jre/bin
 
-    # TODO: move to private
-    if [[ -d /opt/maven ]]; then
-	envpathmunge M2_HOME /opt/maven
-    fi
-    if [[ -d /opt/tomcat ]]; then
-	envpathmunge TOMCATHOME /opt/tomcat
-    fi
-    if [[ -d /opt/ant ]]; then
-	envpathmunge ANTHOME /opt/ant
+	# TODO: move to private
+	if [[ -d /opt/maven ]]; then
+	    envpathmunge M2_HOME /opt/maven
+	fi
+	if [[ -d /opt/tomcat ]]; then
+	    envpathmunge TOMCATHOME /opt/tomcat
+	fi
+	if [[ -d /opt/ant ]]; then
+	    envpathmunge ANTHOME /opt/ant
+	fi
     fi
 fi
