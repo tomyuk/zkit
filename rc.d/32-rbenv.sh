e4355cf7e4067aedd656b98f8c10e1ec5758dbce
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-22 21:11:06 +0900

    Harden Ruby rc under nounset

--- rc.d/32-rbenv.sh
+++ rc.d/32-rbenv.sh
@@ -1,60 +1,44 @@
 # -*- shell-script -*-
 #
-# ruby rvm
+# ruby rvm / rbenv
 #
-
-# if [ -n "${BASH_VERSION:-}" -o -n "${ZSH_VERSION:-}" ] &&
-#     test "`ps -p $$ -o comm=`" != dash &&
-#     test "`ps -p $$ -o comm=`" != sh
-# then
-#     # rbenv
-#     if [[ -d ${HOME}/.rbenv ]]; then
-# 	export PATH="${HOME}/.rbenv/bin:${PATH}"
-# 	if [[ -n "${ZSH_VERSION:-}" ]]; then
-# 	    eval "$( rbenv init - zsh )"
-
-# 	    function gem(){
-# 		$HOME/.rbenv/shims/gem $*
-# 		if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
-# 		then
-# 		    rbenv rehash
-# 		    rehash
-# 		fi
-# 	    }
-
-
-# 	else
-# 	    eval "$( rbenv init - )"
-# 	fi
-#     fi
-# fi
 
 # Ruby環境設定（macOS向け）
 
+homebrew_prefix="${HOMEBREW_PREFIX:-}"
+home_dir="${HOME:-}"
+
 # rbenv設定（Homebrewからインストールした場合）
-if [[ -x ${HOMEBREW_PREFIX}/bin/rbenv ]]; then
-    export RBENV_ROOT="$HOME/.rbenv"
+if [[ -n "$homebrew_prefix" && -x "${homebrew_prefix}/bin/rbenv" && -n "$home_dir" ]]; then
+    export RBENV_ROOT="${home_dir}/.rbenv"
     pathmunge "${RBENV_ROOT}/bin"
-    eval "$(rbenv init - zsh)"
-    
+    if [[ -n "${ZSH_VERSION:-}" ]]; then
+        eval "$(rbenv init - zsh)"
+    else
+        eval "$(rbenv init -)"
+    fi
+
     # gem関数の定義
     function gem(){
-        ${HOMEBREW_PREFIX}/bin/rbenv exec gem $*
-        if [[ "$1" == "install" || "$1" == "i" || "$1" == "uninstall" || "$1" == "uni" ]]; then
+        "${homebrew_prefix}/bin/rbenv" exec gem "$@"
+        if [[ "${1:-}" == "install" || "${1:-}" == "i" || "${1:-}" == "uninstall" || "${1:-}" == "uni" ]]; then
             rbenv rehash
-            rehash
+            if [[ -n "${ZSH_VERSION:-}" ]]; then
+                rehash
+            fi
         fi
     }
 fi
 
 # rvm設定（従来の設定も維持）
-if [[ -d ${HOME}/.rvm ]] && [[ -z $RBENV_ROOT ]]; then
-    source ${HOME}/.rvm/scripts/rvm
+if [[ -n "$home_dir" && -d "${home_dir}/.rvm" && -z "${RBENV_ROOT:-}" ]]; then
+    source "${home_dir}/.rvm/scripts/rvm"
 fi
 
 # Homebrew Ruby設定
-if [[ -d ${HOMEBREW_PREFIX}/opt/ruby ]]; then
-    pathmunge "${HOMEBREW_PREFIX}/opt/ruby/bin"
-    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=${HOMEBREW_PREFIX}/opt/openssl@1.1"
+if [[ -n "$homebrew_prefix" && -d "${homebrew_prefix}/opt/ruby" ]]; then
+    pathmunge "${homebrew_prefix}/opt/ruby/bin"
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=${homebrew_prefix}/opt/openssl@1.1"
 fi
 
+unset homebrew_prefix home_dir
