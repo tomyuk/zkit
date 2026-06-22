e737931eb5642f91032c80892ceb501c393392c6
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-22 20:41:33 +0900

    Harden git bash rc under nounset

--- rc.d/22-git.bash
+++ rc.d/22-git.bash
@@ -3,11 +3,11 @@
 # git
 #
 
-if [[ $PS1 ]]; then
+if [[ -n "${PS1:-}" ]]; then
     # git prompt
 
-    if [[ -r $ZKIT/lib/git-prompt.sh ]]; then
-	source $ZKIT/lib/git-prompt.sh
+    if [[ -r "${ZKIT:-}/lib/git-prompt.sh" ]]; then
+	source "${ZKIT}/lib/git-prompt.sh"
 	GIT_PS1_SHOWDIRTYSTATE=yes
 	GIT_PS1_SHOWSTASHSTATE=yes
 	GIT_PS1_SHOWUNTRACKEDFILES=yes
