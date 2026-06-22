cf2c8f46e28f2196f6537b9e7715e146f692b0d1
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-22 21:11:50 +0900

    Harden Go rc under nounset

--- rc.d/35-go.sh
+++ rc.d/35-go.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 # Go環境設定（macOS向け）
 
+homebrew_prefix="${HOMEBREW_PREFIX:-}"
+home_dir="${HOME:-}"
+
 # Go設定
-if [[ -x ${HOMEBREW_PREFIX}/bin/go ]]; then
-    export GOROOT=${HOMEBREW_PREFIX}/opt/go/libexec
-    export GOPATH="$HOME/go"
-    pathmunge "${GOPATH}/bin"
+if [[ -n "$homebrew_prefix" && -x "${homebrew_prefix}/bin/go" ]]; then
+    export GOROOT="${homebrew_prefix}/opt/go/libexec"
+    if [[ -n "$home_dir" ]]; then
+        export GOPATH="${home_dir}/go"
+        pathmunge "${GOPATH}/bin"
+    fi
     pathmunge "${GOROOT}/bin"
 elif [[ -x /usr/local/go/bin/go ]]; then
     pathmunge "/usr/local/go/bin"
 fi
+
+unset homebrew_prefix home_dir
