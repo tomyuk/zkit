8cd9c54e9c41ac65a8e8120dc93831cb3ec4929f
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-22 13:44:04 +0900

    Guard zsh interactive setup under nounset

--- rc.d/80-interactive.zsh
+++ rc.d/80-interactive.zsh
@@ -2,7 +2,7 @@
 #
 # 対話環境時の設定
 
-if [[ -n $PS1 ]]; then
+if [[ -n "${PS1:-}" ]]; then
     bindkey -e
     bindkey -d
     bindkey ' ' magic-space
@@ -17,11 +17,13 @@ if [[ -n $PS1 ]]; then
     zstyle ':zle:*' skip-whitespace-first false	# wordに後続する空白はまずとばす
 
     # autojump
-    source ${ZKIT}/lib/autojump.zsh
+    if [[ -r "${ZKIT:-}/lib/autojump.zsh" ]]; then
+        source "${ZKIT}/lib/autojump.zsh"
+    fi
     array_pack -r fpath			# 追加された重複するものを後方優先で取り除く
 
     # run-help
-    unalias run-help
+    unalias run-help 2>/dev/null || true
     autoload -Uz run-help
 
     FCEDIT="emacs -nw -Q --eval '(setq backup-inhibited t)'"
