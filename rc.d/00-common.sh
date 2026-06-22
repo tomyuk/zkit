278c6e9debdba88be1818b63aeb0257e8b03d004
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-22 20:46:30 +0900

    Harden common rc path defaults under nounset

--- rc.d/00-common.sh
+++ rc.d/00-common.sh
@@ -3,10 +3,15 @@
 csi=$'\e['
 nl=$'\n'
 
+zkit_root="${ZKIT:-${HOME:-}/.zkit}"
+zkit_private="${ZKIT_PRIVATE:-${HOME:-}/.zkit_private}"
+
 # 90-private.zsh reuse this
 __zkit_path=(
-    ${ZKIT_PRIVATE}/bin
-    ${ZKIT}/bin
-    ${HOME}/.local/bin
-    ${HOME}/bin
+    "${zkit_private}/bin"
+    "${zkit_root}/bin"
+    "${HOME:-}/.local/bin"
+    "${HOME:-}/bin"
 )
+
+unset zkit_root zkit_private
