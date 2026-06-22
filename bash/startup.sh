e6a76992a78af4a6c1a5b656d48b5cda07ad2426
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-22 18:25:47 +0900

    Harden bash startup under nounset

--- bash/startup.sh
+++ bash/startup.sh
@@ -1,47 +1,50 @@
 # このファイルは通常対話環境時にのみ実行されるが、
 # 非対話的スクリプトが BASH_ENV の指定により読み込む場合も想定して記述する。
 
-export ZKIT_PRIVATE=${ZKIT_PRIVATE=${HOME}/.zkit_private}
+: "${ZKIT:=${HOME:-}/.zkit}"
+: "${ZKIT_DEBUG:=false}"
+export ZKIT_PRIVATE="${ZKIT_PRIVATE:-${HOME:-}/.zkit_private}"
 
-umask ${zkit_umask:=0077}
+umask "${zkit_umask:-0077}"
 
-source ${ZKIT}/bash/functions/zkit_core
+source "${ZKIT}/bash/functions/zkit_core"
 require utils
 require pathmunge
 
 # bash<4.2 cannot declare global in function
-if [[ $BASH_VERSION != 3.* ]];then
-    source ${ZKIT}/bash/functions/colors.zkit
+if [[ "${BASH_VERSION:-}" != 3.* ]]; then
+    source "${ZKIT}/bash/functions/colors.zkit"
 fi
 
 # intrinsics
-d=${ZKIT_PRIVATE}/intrinsics
-hostname=$(hostname)
-host=${d}/host-${hostname}
-if [[ ! -r ${host} ]]; then
-    host=${d}/host-${hostname%%.*}
-    if [[ ! -r ${host} ]]; then
-	host=
+d="${ZKIT_PRIVATE}/intrinsics"
+hostname="$(hostname)"
+host="${d}/host-${hostname}"
+if [[ ! -r "${host}" ]]; then
+    host="${d}/host-${hostname%%.*}"
+    if [[ ! -r "${host}" ]]; then
+	host=""
     fi
 fi
-if [[ -n ${host} ]]; then
-    source ${host}
+if [[ -n "${host}" ]]; then
+    source "${host}"
 fi
 unset d hostname host
 
 
 function __zkit_sort_rc () {
-    for rc in $(shopt -s nullglob; echo $*); do
-	echo $(basename $rc) $rc
-    done | sort | cut -d' ' -f2
+    local rc
+    for rc in $(shopt -s nullglob; echo "$@"); do
+	echo "$(basename "$rc")" "$rc"
+    done | sort | cut -d' ' -f2-
 }
 
 function __zkit_load_rc () {
     local __i
-    for __i in $(__zkit_sort_rc $*); do
+    for __i in $(__zkit_sort_rc "$@"); do
 	if [[ -r "$__i" ]]; then
-	    __zkit_msg "load:" $__i
-            if [[ -n $PS1 ]] || $ZKIT_DEBUG; then
+	    __zkit_msg "load:" "$__i"
+            if [[ -n "${PS1:-}" ]] || ${ZKIT_DEBUG:-false}; then
 		source "$__i"
             else
 		source "$__i" >/dev/null 2>&1
@@ -51,5 +54,5 @@ function __zkit_load_rc () {
 }
 
 # 対話型動作の場合のみ rc.d スクリプトの出力を表示する。
-__zkit_load_rc ${ZKIT}/rc.d/*.sh ${ZKIT}/rc.d/*.bash
-__zkit_load_rc ${ZKIT_PRIVATE}/rc.d/*.sh ${ZKIT_PRIVATE}/rc.d/*.bash
+__zkit_load_rc "${ZKIT}"/rc.d/*.sh "${ZKIT}"/rc.d/*.bash
+__zkit_load_rc "${ZKIT_PRIVATE}"/rc.d/*.sh "${ZKIT_PRIVATE}"/rc.d/*.bash
