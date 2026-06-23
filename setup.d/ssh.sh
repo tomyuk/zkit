718936eb805a911576875d1fbb45536ab1d21a5a
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-23 10:12:27 +0900

    Harden SSH setup defaults

--- setup.d/ssh.sh
+++ setup.d/ssh.sh
@@ -2,37 +2,44 @@
 # setup.d/ssh.sh
 #
 
-ssh_dir=${HOME}/.ssh
-if [[ ! -d $ssh_dir ]]; then
-    mkdir $ssh_dir
+home_dir="${HOME:-}"
+zkit_private="${ZKIT_PRIVATE:-${home_dir}/.zkit_private}"
+
+ssh_dir="${home_dir}/.ssh"
+if [[ -n "$home_dir" && ! -d "$ssh_dir" ]]; then
+    mkdir "$ssh_dir"
 fi
 
 
 ## build ssh_config
 
-if [[ $ZSH_VERSION != 4.* ]]; then
+if [[ -n "$home_dir" && "${ZSH_VERSION:-}" != 4.* ]]; then
 
-    : ${SSH_FORWARD_AGENT:=no}
+    : "${SSH_FORWARD_AGENT:=no}"
 
     typeset -a HOSTS
     HOSTS_CONF=()
 
-    hosts_conf=${ZKIT_PRIVATE}/share/hosts.conf
-    if [[ -r $hosts_conf ]]; then
-	__zkit_readarray HOSTS_CONF < ${hosts_conf}
+    hosts_conf="${zkit_private}/share/hosts.conf"
+    if [[ -r "$hosts_conf" ]]; then
+	__zkit_readarray HOSTS_CONF < "$hosts_conf"
     fi
-    __zkit_template templates/ssh_config.tmpl ${ssh_dir}/config 600
+    __zkit_template templates/ssh_config.tmpl "${ssh_dir}/config" 600
 
-elif [[ -s ${ZKIT_PRIVATE}/share/ssh_config ]]; then
-    install -m 600 ${ZKIT_PRIVATE}/share/ssh_config ${ssh_dir}/config
+elif [[ -n "$home_dir" && -s "${zkit_private}/share/ssh_config" ]]; then
+    install -m 600 "${zkit_private}/share/ssh_config" "${ssh_dir}/config"
 fi
 
 ## place authorized_keys
 
-if [[ -s ${ZKIT_PRIVATE}/secrets/authorized_keys ]]; then
-    install -m 600 ${ZKIT_PRIVATE}/secrets/authorized_keys $ssh_dir/
+if [[ -n "$home_dir" && -s "${zkit_private}/secrets/authorized_keys" ]]; then
+    install -m 600 "${zkit_private}/secrets/authorized_keys" "$ssh_dir/"
 fi
 
 ## house keeping
 
-chmod -R og-rwx $ssh_dir
+if [[ -n "$home_dir" && -d "$ssh_dir" ]]; then
+    chmod -R og-rwx "$ssh_dir"
+fi
+
+unset home_dir zkit_private ssh_dir hosts_conf
