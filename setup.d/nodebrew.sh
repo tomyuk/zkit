210c0b05ab7d6aebb7b2d50ffa8a011d902e9044
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-23 09:10:54 +0900

    Harden nodebrew setup defaults

--- setup.d/nodebrew.sh
+++ setup.d/nodebrew.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-: ${nodejs_default_version:=v0.10.12}
+: "${nodejs_default_version:=v0.10.12}"
+home_dir="${HOME:-}"
 
-if [[ ! -d ${HOME}/.nodebrew ]]; then
+if [[ -n "$home_dir" && ! -d "${home_dir}/.nodebrew" ]]; then
 
     curl -L git.io/nodebrew | perl - setup
 
-    export PATH=$HOME/.nodebrew/current/bin:$PATH
+    export PATH="${home_dir}/.nodebrew/current/bin:${PATH:-}"
     hash -r
 
     # build & install default node.js
-    nodebrew install-binary ${nodejs_default_version}
-    nodebrew alias default ${nodejs_default_version}
+    nodebrew install-binary "${nodejs_default_version}"
+    nodebrew alias default "${nodejs_default_version}"
     nodebrew use default
     hash -r
 
     # install npm
     curl https://npmjs.org/install.sh | bash
-    
+
 fi
+
+unset home_dir
