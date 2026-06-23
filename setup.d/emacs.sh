d3e1bd1f1f0348966076273346b5cf5f1fdd3c02
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-23 10:12:14 +0900

    Harden Emacs setup defaults

--- setup.d/emacs.sh
+++ setup.d/emacs.sh
@@ -2,20 +2,24 @@
 # setup.d/emacs
 #
 
-: ${ZKIT_EMACS_D:=${HOME}/.emacs.d}
+home_dir="${HOME:-}"
+: "${ZKIT_EMACS_D:=${home_dir}/.emacs.d}"
 
-if [[ -d ${ZKIT_EMACS_D}/.git ]]; then
-    ( cd ${ZKIT_EMACS_D} && __zkit_run git pull )
+if [[ -d "${ZKIT_EMACS_D}/.git" ]]; then
+    ( cd "${ZKIT_EMACS_D}" && __zkit_run git pull )
 else
-    if [[ -n $ZKIT_EMACS_REPO ]]; then
-	if [[ -d ${ZKIT_EMACS_D} ]]; then
-	    mv ${ZKIT_EMACS_D} ${ZKIT_EMACS_D}.$$.bak
+    if [[ -n "${ZKIT_EMACS_REPO:-}" ]]; then
+	if [[ -d "${ZKIT_EMACS_D}" ]]; then
+	    mv "${ZKIT_EMACS_D}" "${ZKIT_EMACS_D}.$$.bak"
 	fi
-	__zkit_run git clone $ZKIT_EMACS_REPO ${ZKIT_EMACS_D}
+	__zkit_run git clone "${ZKIT_EMACS_REPO}" "${ZKIT_EMACS_D}"
     fi
 fi
 
-__zkit_install ${HOME}/.emacs.d/dot.emacs ${HOME}/.emacs
+if [[ -n "$home_dir" ]]; then
+    __zkit_install "${home_dir}/.emacs.d/dot.emacs" "${home_dir}/.emacs"
+fi
 
-#(cd $ZKIT_EMACS_D && git submodule update --init)
+unset home_dir
 
+#(cd "$ZKIT_EMACS_D" && git submodule update --init)
