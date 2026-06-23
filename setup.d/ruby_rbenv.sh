24b0add69841976e2b33c882613b7ff5bebe44cb
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-23 09:11:02 +0900

    Harden ruby rbenv setup defaults

--- setup.d/ruby_rbenv.sh
+++ setup.d/ruby_rbenv.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-. ${ZKIT}/setup.d/ruby.sh
+. "${ZKIT:-${HOME:-}/.zkit}/setup.d/ruby.sh"
 
-if [[ ! -d ${HOME}/.rbenv ]]; then
-    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-    export PATH="$HOME/.rbenv/bin:$PATH"
+home_dir="${HOME:-}"
+
+if [[ -n "$home_dir" && ! -d "${home_dir}/.rbenv" ]]; then
+    git clone https://github.com/sstephenson/rbenv.git "${home_dir}/.rbenv"
+    export PATH="${home_dir}/.rbenv/bin:${PATH:-}"
     eval "$(rbenv init -)"
 fi
 
-if [[ ! -f ${HOME}/.local/bin/ruby-build ]]; then
+if [[ -n "$home_dir" && ! -f "${home_dir}/.local/bin/ruby-build" ]]; then
     tmpdir=$(mktemp -d /tmp/zkit.XXXXXX )
     (
-	cd $tmpdir
+	cd "$tmpdir" || exit 1
 	git clone https://github.com/sstephenson/ruby-build.git
-	cd ruby-build
-	PREFIX=~/.local ./install.sh
+	cd ruby-build || exit 1
+	PREFIX="${home_dir}/.local" ./install.sh
     )
-    rm -rf $tmpdir
+    rm -rf "$tmpdir"
     unset tmpdir
 fi
 
-if [[ ! -d ${HOME}/.rbenv/versions/${default_ruby_version} ]]; then
-    rbenv install ${default_ruby_version}
+if [[ -n "$home_dir" && -n "${default_ruby_version:-}" && ! -d "${home_dir}/.rbenv/versions/${default_ruby_version}" ]]; then
+    rbenv install "${default_ruby_version}"
 fi
+
+unset home_dir
